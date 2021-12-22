4cc53dd0ef12a549e124f47b543d2d4f8c91fbbd
Reza Shabrang <rezashabrang.m@gmail.com>
2021-12-22 15:05:19 +0330

    Writing cleaner.

---
+++ phrase_counter/cleaner.py
@@ -0,0 +1,58 @@
+"""Cleaning dirty input text."""
+import re
+
+import requests
+from bitas_utils import clear_stop_char, replace_arabic_char
+from bs4 import BeautifulSoup
+
+
+def fetch_page_text(url: str = "", webpage: str = "") -> str:
+    """Getting html pages from urls & fetching needed elements of the page.
+
+    Args:
+        url: url of the target webpage.
+        webpage: string version of the webpage.
+
+    Returns:
+        text of the webpage.
+    """
+    # If url is defined then make the request and fetch the page.
+    if url != "":
+        req = requests.get(url=url)
+        soup = BeautifulSoup(req.content, "html.parser")
+
+    # If not then make soup from given webpage.
+    else:
+        soup = BeautifulSoup(webpage, "html.parser")
+
+    # Getting needed tags and stripped strings
+    whole_page = ""
+    for group in soup(["h1", "h2", "h3", "h4", "h5", "h6", "p"]):
+        for text in group.stripped_strings:
+            whole_page += text + " "
+
+    whole_page = whole_page.strip()
+
+    return whole_page
+
+
+def cleaner(dirty_text: str) -> str:
+    """Main function for cleaning.
+
+    Args:
+        dirty_text: Input dirty text
+
+    Returns:
+        Final text ready for integration in NLP algorithms.
+    """
+    # ------------------- Linguistic phase -------------------
+    processed_text = replace_arabic_char(dirty_text)
+    processed_text = clear_stop_char(processed_text)
+
+    # ------------------- Trimmer phase -------------------
+    processed_text = processed_text.replace("\t", " ").replace("\n", " ").strip()
+    processed_text = processed_text.replace("\u200c", " ")  # Nim-fasele
+    processed_text = re.sub(" +", " ", processed_text)  # space cleaner
+    processed_text = processed_text.strip()
+
+    return str(processed_text)
---
+++ tests/test_cleaner.py
@@ -0,0 +1,30 @@
+from phrase_counter.cleaner import cleaner, fetch_page_text
+
+
+def test_fetch_html_returns_true():
+    """
+    Testing that fetch_page_text returns something.
+    """
+    webpage = "<p>hello there</p>"
+    page = fetch_page_text(
+        webpage=webpage
+    )
+    assert page
+
+
+def test_extra_space_cleaner():
+    """
+    Testing that cleaner removes extra spaces.
+    """
+    dirty_text = "    there is     lots  of     spaces       here      "
+    cleaned_text = cleaner(dirty_text)
+    assert cleaned_text == "there is lots of spaces here"
+
+
+def test_new_line_and_tab_cleaner():
+    """
+    Testing that the trimmer removes newlines and tabs.
+    """
+    dirty_text = "There is \t lots of \n newlines \n and \t tabs \n here"
+    cleaned_text = cleaner(dirty_text)
+    assert cleaned_text == "There is lots of newlines and tabs here"
