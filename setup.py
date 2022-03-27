c745c437bf7e5ca5c7178ea028560143debb0f13
Reza Shabrang <rezashabrang.m@gmail.com>
2022-03-27 07:43:06 +0430

    Adding setup.py Adding Pypi pipeline

---
+++ setup.py
@@ -0,0 +1,63 @@
+"""setup the package."""
+import sys
+
+import setuptools
+
+needs_pytest = {"ptr", "pytest", "test"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
+
+setuptools.setup(
+    name="common-phrase-detection",
+    version="0.1.0",
+    author="Reza Shabrang",
+    author_email="rezashabrang.m@gmail.com",
+    url="https://github.com/rezashabrang/common-phrase-detection",
+    description="""A package for counting ngrams in a text.""",
+    license="MIT",
+    packages=setuptools.find_packages(
+        exclude=["_common_phrase_detection_tests", "*.__pycache__"]
+    ),
+    python_requires=">=3.8",
+    install_requires=[
+        "cleaning-utils", "beautifulsoup4", "requests", "scikit-learn",
+        "pandas", "polyglot", "PyICU", "pycld2"
+    ],
+    tests_require=["pytest"],
+    extras_require={
+        "tests": [
+            "flake8",
+            "pytest",
+            "pytest-cov",
+            "pytest-flake8",
+            "pip",
+            "bandit",
+            "black",
+            "coverage",
+            "coverage-badge",
+            "darglint",
+            "isort",
+            "mypy",
+            "mypy-extensions",
+            "pre-commit",
+            "pydocstyle",
+            "pylint",
+            "pytest-html",
+            "pyupgrade",
+            "safety",
+            "radon",
+            "docstr-coverage",
+        ],
+    },
+    setup_requires=[] + pytest_runner,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
